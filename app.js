39967736acbebd579e967341c26ea031d46c7736
Hugo-Duran-Toro <duran.toro.hugo@gmail.com>
2025-09-01 00:38:10 -0400

    actualizacion de app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+let nombre
+let edad
+let lenguajeProgramacion
 
+const nombre = prompt("Indicanos tu nombre, por favor:");
+
+const msg = `¡Que tal ${nombre}!`;
+
+alert(msg);
+
+const edad = prompt("Indicanos tu edad, por favor:");
+
+const msg = `¡Tu edad es: ${edad}!`;
+
+alert(msg);
+
+const nombre = prompt("Indicanos tu lenguaje de programacion de preferencia, por favor:");
+
+const msg = `y quieres estudiar programacion con enfoque en ${lenguajeProgramacion}`;
+
+alert(msg);
