f9024de2338c39f6970d67896fddc615caef2174
Hugo-Duran-Toro <duran.toro.hugo@gmail.com>
2025-09-01 00:49:36 -0400

    Actualizacion final app.js

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-let nombre
-let edad
-let lenguajeProgramacion
-
 const nombre = prompt("Indicanos tu nombre, por favor:");
 
-const msg = `¡Que tal ${nombre}!`;
-
-alert(msg);
 
 const edad = prompt("Indicanos tu edad, por favor:");
 
-const msg = `¡Tu edad es: ${edad}!`;
 
-alert(msg);
+const lenguajeProgramacion = prompt("Indicanos tu lenguaje de programacion de preferencia, por favor:");
 
-const nombre = prompt("Indicanos tu lenguaje de programacion de preferencia, por favor:");
+const msg3 = `Hola ${nombre}, en tus ${edad} años de edad, estas en un muy buen momento para estudiar programacion con enfoque en lenguaje ${lenguajeProgramacion}`;
 
-const msg = `y quieres estudiar programacion con enfoque en ${lenguajeProgramacion}`;
-
-alert(msg);
+alert(msg3);
